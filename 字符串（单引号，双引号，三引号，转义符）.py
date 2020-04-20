d79e288d31678962ad90bee1286118b7aa8453cf
Mr.Chen <40707704+wu95063@users.noreply.github.com>
2020-04-20 14:10:27 +0800

    增加了关于中括号的使用方法。

--- 字符串（单引号，双引号，三引号，转义符）.py
+++ 字符串（单引号，双引号，三引号，转义符）.py
@@ -10,3 +10,23 @@ github.
                                             your sincerely
                                             Michael Chen
 ''')#（'''  '''）或者（""""""）三引号支持多行的书写，如果我们需要书信的格式，可以运用三引号来输出字符串。
+#关于python再字符串中的一些特定--中括号的使用，即为一种---索引功能。
+name='python'
+    # 012345
+    # -6-5-4-3-2-1
+print(name[0])#->p
+print(name[-1])#->n
+print(name[-6])#->p
+print(name[0:3])#->pyt
+print(name[0:-1])#->pytho(不包括【-1】)
+print(name[0:10])#->python
+print(name[0:6])#->python
+print(name[0:5])#->pytho
+print(name[0:100])#->python
+print(name[-1:-10])#->输出为空=print('')
+print(name[:])#->python
+another=name[:]#定义一个变量。
+print(another)#->python
+
+
+
