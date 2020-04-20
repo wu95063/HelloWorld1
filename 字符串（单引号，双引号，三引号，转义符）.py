a18462e1911b092fb1a6d9f788d7d1d2d3e73a59
Mr.Chen <40707704+wu95063@users.noreply.github.com>
2020-04-20 13:46:05 +0800

    关于字符串的一些说明。

---
+++ 字符串（单引号，双引号，三引号，转义符）.py
@@ -0,0 +1,12 @@
+print('what is your name? ')#单引号内输入字符串
+print('what\'s your name? ')#当单引号内部出现单引号的时候，需要运用转义符(\).
+print("what's your name? ")#双引号内输入字符串
+print('The question is "what is your name "')#单引号内部出现双引号是允许的，并且双引号变为字符串。
+print('''
+Hello you guys,
+    My name is Michael Chen and i'm so happy to join
+github.
+    So let's get started.
+                                            your sincerely
+                                            Michael Chen
+''')#（'''  '''）或者（""""""）三引号支持多行的书写，如果我们需要书信的格式，可以运用三引号来输出字符串。
